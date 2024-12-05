45bb743f6cd5e75269672eba97ab442908bd0d90
Rajitha <rajithapulasthi11@gmail.com>
2024-12-05 21:02:17 +0530

    Tailwind Install and Header Div added

---
+++ smart todo list/src/components/HeaderSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const headerSection = () => {
+  return (
+    <div className='w-full h-[15vh] bg-blue-800'>
+      
+    </div>
+  )
+}
+
+export default headerSection
---
+++ smart todo list/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeaderSecton from '../components/headerSection'
+const home = () => {
+  return (
+    <div>
+      <HeaderSecton />
+    </div>
+  )
+}
+
+export default home
